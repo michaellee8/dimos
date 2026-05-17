67be2b9788dbd47f143ff37331aed4bcb3f61586
Ivan Nikolic <lesh@sysphere.org>
2026-05-17 20:21:51 +0800

    reloc autoresearch sketch

---
+++ dimos/mapping/relocalization/run.py
@@ -0,0 +1,20 @@
+# Copyright 2026 Dimensional Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ... execute relocalize.py, summarize results, kill if too slow
+
+print("---")
+print(f"total_seconds:    {t_end - t_start:.1f}")
+print(f"average_distance:     {peak_vram_mb:.1f}")
+print("all_distances:     []")
