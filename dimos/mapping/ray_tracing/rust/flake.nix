166e160e8ec525c291c6d292b3370d6d039639e5
Andrew Lauer <andrewcl@email.unc.edu>
2026-06-23 11:30:56 -0700

    Fix flake

--- dimos/mapping/ray_tracing/rust/flake.nix
+++ dimos/mapping/ray_tracing/rust/flake.nix
@@ -6,8 +6,8 @@
     flake-utils.url = "github:numtide/flake-utils";
     # Relative git+file: will be deprecated (nix#12281) but there's no
     # viable alternative for reaching local path deps outside the flake dir currently
-    # presumably an alternative will be added before this is removed
-    dimos-repo = { url = "git+file:../../../.."; flake = false; };
+    # presumably an alternative will be added before this is removed.
+    dimos-repo = { url = "git+file:../../../..?ref=main"; flake = false; };
   };
 
   outputs = { self, nixpkgs, flake-utils, dimos-repo }:
